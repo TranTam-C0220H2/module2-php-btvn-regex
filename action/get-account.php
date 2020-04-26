607abbec223371a6c79871c5b9b16d9c05609687
TranTam-C0220H2 <tam.tvt.ptit@gmail.com>
2020-04-26 15:27:15 +0700

    up

--- action/get-account.php
+++ action/get-account.php
@@ -3,19 +3,20 @@ include '../class/UserManager.php';
 session_start();
 $phoneNumber = $_POST['phoneNumber'];
 $userName = $_POST['userName'];
-$_SESSION['userName'] = $userName;
+$_SESSION['userName'] = '';
 $userManager = new UserManager();
 $_SESSION['dataArrayUser'] = $userManager->getDataJsonUser('../data/data.json');
 foreach ($_SESSION['dataArrayUser'] as $item) {
+
+
     if ($item->phoneNumber == $phoneNumber && $item->email == $userName) {
         header('Location: ../view/new-password.php');
     } else {
         if ($item->email == $userName) {
+            $_SESSION['userName'] = $userName;
             $_SESSION['phoneNumber'] = false;
-        } else {
-            $_SESSION['userName'] = '';
         }
-        header('Location: ../view/forget-password.php');
     }
+    header('Location: ../view/forget-password.php');
 }
 
